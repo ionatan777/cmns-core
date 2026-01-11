bf3cce1f524a9d2d087508fb8346c4b55c1dd753
Jhonatan Pillajo <jhonatanpillajo79@gmail.com>
2026-01-10 19:16:23 -0500

    fix: Hardcode Supabase keys to resolve Vercel config issue

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    'https://juooxzbexwkgvofkeare.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1b294emJleHdrZ3ZvZmtlYXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0MDE5MjMsImV4cCI6MjA4Mjk3NzkyM30.lZkDicFqR1UGER031sqci3dE0Tl5qu9rhgqIXNB6910'
   )
 }
